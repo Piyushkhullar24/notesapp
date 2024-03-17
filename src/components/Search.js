8a7e8b72cc77195b346b5ae87e308351307c173d
piyush khullar <khullar.piyush24@gmail.com>
2024-03-17 17:40:13 +0530

    initial

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import { MdSearch } from "react-icons/md";
+
+const Search = ({handleSearchNote}) => {
+
+    return <div className="search">
+        <MdSearch className="search-icons" size='1.3rem' />
+        <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="type to search...."></input>
+    </div>;
+}
+
+export default Search;
